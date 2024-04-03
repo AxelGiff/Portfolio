01ab2e247f6233e69e190a2bfe18a621982706ec
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2024-04-03 23:49:38 +0200

    Add files via upload

--- src/ButtonRotatingBackgroundGradient.tsx
+++ src/ButtonRotatingBackgroundGradient.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const ButtonRotatingBackgroundGradient = ({text}) => {
+const ButtonRotatingBackgroundGradient = ({text,link}) => {
     return (
-      <button className="font-sans text-base items-center h-12 rounded-md font-semibold px-3 text-dark border-none cursor-pointer shadow-md bg-white hover:bg-gray-200">
+  
+     
+      <a href={link} target="_blank" rel="noreferrer" role="button"> 
+      <button type="button" className="font-sans text-base items-center h-12 rounded-md font-semibold px-3 text-dark border-none cursor-pointer shadow-md bg-white hover:bg-gray-200">
       {text}
-    </button>
+    </button></a> 
+  
     );
   };
   
