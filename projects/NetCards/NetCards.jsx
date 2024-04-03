bf288ad2e3a9cb939570d0cad69372f8872da719
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2024-04-03 23:45:39 +0200

    Delete projects directory

--- projects/NetCards/NetCards.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import LandingNetCards from './LandingNetCards.jsx';
-
-export default function PerformVision() {
-  return (
-    <div><LandingNetCards /></div>
-  )
-}
--- projects/SafePage/SafePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import LandingSafePage from './LandingSafePage.jsx';
-
-export default function PerformVision() {
-  return (
-    <div><LandingSafePage /></div>
-  )
-}
