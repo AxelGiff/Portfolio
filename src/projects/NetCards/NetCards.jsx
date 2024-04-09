8a15d038cc921ce7f24027bfad601b5fa3c3b629
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2024-04-09 21:50:39 +0200

    Add files via upload

--- src/projects/NetCards/NetCards.jsx
+++ src/projects/NetCards/NetCards.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import LandingNetCards from './LandingNetCards.jsx';
+import Technologies from '../Technologies.jsx';
+import AboutNetCards from './AboutNetCards.jsx';
+import Others from './Others.jsx';
+import Deck from './Deck.jsx';
 
 export default function PerformVision() {
   return (
-    <div><LandingNetCards /></div>
+    <div className="flex flex-col items-center justify-center ">
+    <LandingNetCards />
+    
+    <img src={require("./images/Netcards.PNG")} width={1200} height={650} alt="screen todo list" loading="lazy" className="-mt-96 -z-[-10] shadow-lg invisible sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" />
+    <Technologies iconsToShow={["HTML","CSS","MySQL","PHP","Figma"]} />
+    <AboutNetCards />
+    <Others />
+    <Deck />
+  </div>
   )
 }
