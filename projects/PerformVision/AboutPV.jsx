ee8a1ee5e189005d2eec3e32b1dfc67550f41578
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2024-04-18 16:00:53 +0200

    Add files via upload

---
+++ projects/PerformVision/AboutPV.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import BadgeTextGradient from '../../BadgeGradient.tsx';
+import ButtonRotatingBackgroundGradient from '../../ButtonRotatingBackgroundGradient.tsx';
+import { TextGenerateEffect } from '../../text-generate-effect.tsx';
+import {LayoutGridDemo} from './layout.tsx';
+
+export default function AboutPv() {
+  const words = `Perform Vision is an intranet site developed as part of my training at I.U.T De Villetaneuse. The objective was to create, with the help of my 4 teammates, a time and material service site allowing the management of delivery notes.`;
+
+  return (
+    <section id="about-container">
+      <h1 className='about-title text-white text-5xl block text-center block mt-24 mb-32'>Zoom-in</h1>
+      <div className="flex flex_responsive flex-col sm:flex-row items-center gap-12 m-48 sm:mt-0 sm:gap-24">
+      <div class="gradient-pv z-[-10000] right-1/4 -top-1/8 w-1/2 h-full opacity-30"></div>
+
+        {/* Image on the left */}
+        <div className="relative w-full sm:w-1/2 h-auto">
+          <div class="gradient-01 right-1/4 -top-1/8 w-1/2 h-full opacity-30"></div>
+          <img alt="Product Preview" loading="lazy" width="2000vw" height="100vw" className="shadow-lg invisible sm:visible rounded-lg [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 relative z-40 opacity-100" style={{color:"transparent"}}  src={require("./images/dashboard_bdl_admin.png")} /> 
+        </div>
+        {/* Text on the right */}
+        <div className="text flex flex-col items-center justify-center z-50 sm:w-1/2">
+          <TextGenerateEffect words={words} />
+          <div className="mt-24 items-center "> 
+            <ButtonRotatingBackgroundGradient text="Link to the Github code" link="https://github.com/AxelGiff/SAE_PerformVision" />
+          </div>
+        </div>
+      </div>
+      <img alt="left gradient" loading="lazy" width="500" height="500" decoding="async" data-nimg="1" className="absolute right-0 top-1/8" style={{color:"transparent"}} src={require("../../images/glow.png")} />
+      <div className="relative">
+        <img alt="gradient" loading="lazy" decoding="async" data-nimg="fill" class="h-[1000px] w-[1000px] inset-0 object-cover absolute pointer-events-none select-none" style={{height:"200vh",width:"auto",left:"0",right:"0",top:"-540px",color:"transparent"}} sizes="100vw" src={require("../../images/tech-features-gradient.webp")} />
+      </div>
+      <LayoutGridDemo />
+    </section>
+  )
+}
---
+++ projects/SafePage/LandingSafePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { SpotlightPreview } from '../../pageSpotlight.tsx';
+import TextShine from '../../TextShine.tsx'; 
+
+import {AnimatedTooltipPreview} from "../PerformVision/tooltip.tsx";
+
+export default function LandingPage() {
+  return (
+    
+    <div className="flex mb-40 -mt-24 font-inter flex-col items-center justify-center h-screen z-10">
+      <div class="gradient-sp z-[-10000]  -top-1/4 w-full h-full opacity-30"></div>
+            <SpotlightPreview fill="purple" />
+         
+        <p className="text-gradient-primary1 text-[1.2rem] font-bold relative right-2 mt-6">2023 | One month <br></br>  <TextShine text="SafePage" />{/* <h1 className="text-gradient-primary2 font-bold text-8xl ">Axel</h1> */}</p>
+        <h2 className=" text-[1.1rem] bg-gradient-to-b from-white via-white to-white bg-clip-text text-transparent text-transparent mt-16">Group work</h2>
+
+   <div className="flex justify-between gap-4">
+    
+   <AnimatedTooltipPreview number="1" old="0" />
+
+</div>
+   {/*   <TextRevealCard
+        text="Front-End Developer"
+        revealText="Front-End Developer" /> */}
+  
+    </div>
+  )
+}
