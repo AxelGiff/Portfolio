8ffb39e072823fad4fd21c10ebb3eb803759496b
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2024-04-20 23:16:03 +0200

    Update deck, ml-24 for centering the deck image

--- src/projects/NetCards/Deck.jsx
+++ src/projects/NetCards/Deck.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Deck () {
   return (
     <div className='flex flex-col items-center justify-center sm:flex-row items-center gap-12 m-48 sm:mt-0 sm:gap-24'>
-                      <img src={require("./images/netcards4.png")} width={1000} height={650} alt="screen todo list" loading="lazy" className="translate-x-1.5 flex justify-center items-center -z-[-10] shadow-lg invisible sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" />
+                      <img src={require("./images/netcards4.png")} width={1000} height={650} alt="screen todo list" loading="lazy" className="translate-x-1.5 flex ml-24 justify-center items-center -z-[-10] shadow-lg invisible sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" />
 
                 <div >
               <h2 className='about-title text-white text-5xl block mr-24 xl:mr-48 text-center block mt-24 mb-12 '>Deck system</h2>
