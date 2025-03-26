e8079ae1de7174c0738f2217605e68b6d01fb6f5
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-26 22:41:50 +0100

    Update cnn.jsx

--- src/projects/CNN/cnn.jsx
+++ src/projects/CNN/cnn.jsx
@@ -12,7 +12,7 @@ export default function Cnn() {
         src={new URL("./images/linus.png", import.meta.url).href}
         alt="screen todo list"
         loading="lazy"
-        className="-mt-96 -z-[-10] w-[100%] h-[100%] sm:w-[75%] sm:h-[650px] shadow-lg sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl"
+        className="-mt-96 -z-[-10] w-[100%] h-[100%] sm:w-[60%] sm:h-[650px] shadow-lg sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl"
       />
       <Technologies iconsToShow={["Scikit-Learn", "Tensorflow", "Keras", "HuggingFace", "Gradio"]} />
       <AboutCnn />
