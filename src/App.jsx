413e699d32438d5d762c70c73b96e6a238dc0023
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-28 23:23:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
+import React, { Suspense, lazy, memo } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './index.css';
 import Header from './Header.jsx';
-import Avatar from './Avatar.jsx';
-import LandingPage from './LandingPage.jsx';
-import About from './About.jsx';
-import History from './History.jsx';
-import Contact from './Contact.jsx';
-import Routage from './Routage.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProjectDetail from './ProjectDetail.jsx'; // Créez ce composant pour afficher les détails d'un projet
-import LanguageSelector from './LanguageSelector.jsx';
-import React, { Suspense, lazy } from 'react';
+import ErrorBoundary from './ErrorBoundary.jsx';
 
+// Lazy load components mais avec un fallback invisible
+const Routage = lazy(() => import('./Routage.jsx'));
+const Projects = lazy(() => import('./Projects.jsx'));
+const ProjectDetail = lazy(() => import('./ProjectDetail.jsx'));
 
-const Projects = lazy(() => import('./Projects'));
-
-function App() {
+const App = memo(() => {
   return (
     <div className="font-apple">
       <Header />
-      <Routes>
-        <Route path="/" element={<Routage />} />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path="/projects" element={ // Route pour le composant Projects lazy loaded
-          <Suspense fallback={<div>Chargement des projets...</div>}>
-            <Projects />
-          </Suspense>
-        } />
-      </Routes>
+      <ErrorBoundary>
+        <Suspense >
+          <Routes>
+            <Route path="/" element={<Routage />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
-}
+});
+
 export default App;
