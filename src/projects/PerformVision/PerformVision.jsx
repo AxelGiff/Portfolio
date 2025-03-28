155871ef621920d25b9c46f407490be067d60bab
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-28 23:26:00 +0100

    Update PerformVision.jsx

--- src/projects/PerformVision/PerformVision.jsx
+++ src/projects/PerformVision/PerformVision.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { memo } from 'react';
 import LandingPV from './LandingPV.jsx';
 import Technologies from '../Technologies.jsx';
 import AboutPv from './AboutPV.jsx';
 
-export default function PerformVision() {
+import screenDashAdminPres from './images/screen_dash_admin_pres.png';
+
+const PerformVision = memo(() => {
+  const technologies = ["HTML", "Tailwind", "PHP", "Javascript", "PostgreSQL", "Figma"];
+  
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center">
       <LandingPV />
-      <img src={new URL("./images/screen_dash_admin_pres.png", import.meta.url).href} alt="screen dashboard perform vision" loading="lazy" className="-mt-96 w-[100%] h-[100%] sm:w-[1200px] sm:h-[650px] -z-[-10] shadow-lg  sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" />
-      <Technologies iconsToShow={["HTML","Tailwind","PHP","Javascript","PostgreSQL","Figma"]} />
+      <img 
+        src={screenDashAdminPres} 
+        alt="screen dashboard perform vision" 
+        loading="lazy" 
+        className="-mt-96 w-[100%] h-[100%] sm:w-[1200px] sm:h-[650px] -z-[-10] shadow-lg sm:visible [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" 
+      />
+      <Technologies iconsToShow={technologies} />
       <AboutPv />
     </div>
-  )
-}
+  );
+});
+
+export default PerformVision;
