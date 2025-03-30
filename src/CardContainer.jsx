1c1a4a6f303c32299bed278983e4439f35d6b48a
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-30 13:01:58 +0200

    Update CardContainer.jsx

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const CardContainer = () => {
 
+  const { t } = useTranslation();
 
   return (
 
 <div className="w-[400px] z-[956] sm:z-[-1000] h-[200px] min-h-[200px] sm:ml-40 arriere absolute top-32 sm:top-60 transform sm:translate-x-48 letter-spacing-1 text-white bg-hsla-45-17-95-80 backdrop-blur-xl backdrop-saturate-17 backdrop-filter-contrast-170 backdrop-filter px-10 border border-gray-800 animate-border-width bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000 bottom-0 rounded-2xl flex items-center justify-center">
       
             <div className="card-content">
-              <h3 className="card-title top-0 text-[#387bdf] font-Inter">Hire me</h3>
+              <h3 className="card-title top-0 text-[#387bdf] font-Inter">{t("Brief presentation")}</h3>
               <p className="card-text text-center leading-10 italic font-thin font-Inter">
-                “I am a web developer passionate about creating interactive online experiences“</p>
+                “{t("card_container")}“</p>
             </div>
         </div>
 /*       
