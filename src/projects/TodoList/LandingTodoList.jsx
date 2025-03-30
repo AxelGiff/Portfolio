5e24f5a0c4e2c4fa5f4792eb055451e087f844fb
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-30 22:54:51 +0200

    Update LandingTodoList.jsx

--- src/projects/TodoList/LandingTodoList.jsx
+++ src/projects/TodoList/LandingTodoList.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { SpotlightPreview } from '../../pageSpotlight.tsx';
 import TextShine from '../../TextShine.tsx'; 
+import { useTranslation } from 'react-i18next'; 
 
 import {AnimatedTooltipPreview} from "../PerformVision/tooltip.tsx";
 
 export default function LandingPage() {
+  const { t } = useTranslation(); 
+
   return (
     
     <div className="flex mb-40 -mt-24 font-inter flex-col items-center justify-center h-screen z-10">
       <div class="gradient-pv z-[-10000]  -top-1/4 w-full h-full opacity-30"></div>
             <SpotlightPreview fill="cyan" />
          
-        <p className="text-gradient-primary1 text-[1.2rem] font-bold relative right-2 mt-6">2023 | One week <br></br>  <TextShine text="Todo List" />{/* <h1 className="text-gradient-primary2 font-bold text-8xl ">Axel</h1> */}</p>
+        <p className="text-gradient-primary1 text-[1.2rem] font-bold relative right-2 mt-6">2023 | {t('todolist.oneweek')} <br></br>  <TextShine text="Todo List" />{/* <h1 className="text-gradient-primary2 font-bold text-8xl ">Axel</h1> */}</p>
         <h2 className=" text-[1.1rem] bg-gradient-to-b from-white via-white to-white bg-clip-text text-transparent text-transparent mt-16">Group work</h2>
 
    <div className="flex justify-between gap-4">
