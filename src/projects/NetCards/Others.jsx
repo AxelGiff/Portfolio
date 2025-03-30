03cf2d845c3ab0fcf738bbc464eea828bfdf3b61
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-30 22:54:09 +0200

    Update Others.jsx

--- src/projects/NetCards/Others.jsx
+++ src/projects/NetCards/Others.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-export default function Others () {
+export default function Others() {
+  const { t } = useTranslation();
+  
   return (
     <div className='flex flex-col items-center justify-center sm:flex-row items-center gap-12 m-64 xl:m-48 sm:mt-0 sm:gap-24'>
-                <div >
-              <h2 className='about-title text-white text-5xl block ml-8 xl:ml-56 text-center block mt-24 mb-12 '>Authentication system</h2>
-              <h2 className='about-title text-white italic text-xl ml-16 xl:ml-64 leading-2 mb-24'>An authentication system has been designed and deployed with the primary objective of enabling users to securely log in and access their personalized decks of flashcards. By incorporating this system, users can enjoy a seamless experience, tailored to their individual preferences and learning needs. Additionally, this authentication mechanism ensures the privacy and security of user data, fostering trust and reliability within the platform.</h2>
-              </div>
-              <img src={new URL("./images/sinscrire.png", import.meta.url).href} width={1100} height={650} alt="screen todo list" loading="lazy" className=" flex justify-center items-center -z-[-10] shadow-lg  sm:visible translate-x-1.5 [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" />
-              
-              <div>
-
-              </div>
+      <div>
+        <h2 className='about-title text-white text-5xl block ml-8 xl:ml-56 text-center block mt-24 mb-12'>
+          {t('netcards.auth.title')}
+        </h2>
+        <h2 className='about-title text-white italic text-xl ml-16 xl:ml-64 leading-2 mb-24'>
+          {t('netcards.auth.description')}
+        </h2>
+      </div>
+      
+      <img 
+        src={new URL("./images/sinscrire.png", import.meta.url).href} 
+        width={1100} 
+        height={650} 
+        alt={t('netcards.auth.imageAlt')} 
+        loading="lazy" 
+        className="flex justify-center items-center -z-[-10] shadow-lg sm:visible translate-x-1.5 [mask-image:linear-gradient(to_bottom,white,white,transparent)] transition duration-1000 rounded-xl" 
+      />
+      
+      <div></div>
     </div>
-  )
+  );
 }
