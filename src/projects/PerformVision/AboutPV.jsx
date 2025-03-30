47dd7fbe8ff79a1c08ebf2ae26cd0b3a1f4134c0
Axel Giffard <101025474+AxelGiff@users.noreply.github.com>
2025-03-30 18:22:14 +0200

    Update AboutPV.jsx

--- src/projects/PerformVision/AboutPV.jsx
+++ src/projects/PerformVision/AboutPV.jsx
@@ -3,20 +3,25 @@ import BadgeTextGradient from '../../BadgeGradient.tsx';
 import ButtonRotatingBackgroundGradient from '../../ButtonRotatingBackgroundGradient.tsx';
 import { TextGenerateEffect } from '../../text-generate-effect.tsx';
 import { LayoutGridDemo } from './layout.tsx';
+import { useTranslation } from 'react-i18next';
 
+// Import images directly
 import dashboardBdlAdmin from './images/dashboard_bdl_admin.png';
 import glowImage from '../../images/glow.png';
 import techFeaturesGradient from '../../images/tech-features-gradient.webp';
 
 const AboutPv = memo(() => {
-  const words = `Perform Vision is an intranet site developed as part of my training at I.U.T De Villetaneuse. The objective was to create, with the help of my 4 teammates, a time and material service site allowing the management of delivery notes.`;
+        const { t } = useTranslation();
+  
+  const words = t('performvision_description');
 
   return (
     <section id="about-container">
-      <h1 className='about-title text-white text-5xl block text-center block mt-24 mb-32'>Zoom-in</h1>
+      <h1 className='about-title text-white text-5xl block text-center block mt-24 mb-32'>{t('Zoom-in')}</h1>
       <div className="flex flex_responsive flex-col sm:flex-row items-center gap-12 m-48 sm:mt-0 sm:gap-24">
         <div className="gradient-pv z-[-10000] right-1/4 -top-1/8 w-1/2 h-full opacity-30"></div>
 
+        {/* Image on the left */}
         <div className="relative w-full sm:w-1/2 h-auto">
           <div className="gradient-01 right-1/4 -top-1/8 w-1/2 h-full opacity-30"></div>
           <img 
@@ -28,10 +33,11 @@ const AboutPv = memo(() => {
             src={dashboardBdlAdmin} 
           /> 
         </div>
+        {/* Text on the right */}
         <div className="text flex flex-col items-center justify-center z-50 sm:w-1/2">
           <p className="text-white text-lg">{words}</p>                 
           <div className="mt-24 items-center "> 
-            <ButtonRotatingBackgroundGradient text="Link to the Github code" link="https://github.com/AxelGiff/SAE_PerformVision" />
+            <ButtonRotatingBackgroundGradient text={t('Github')} link="https://github.com/AxelGiff/SAE_PerformVision" />
           </div>
         </div>
       </div>
